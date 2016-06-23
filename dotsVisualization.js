a7078c98ce77e888e622d22b74b5038fd4310859
marina fortes rey <lalai_sama@hotmail.com>
2016-06-23 15:37:58 -0300

    fixing dots, ainda nao esta 100%

--- dotsVisualization.js
+++ dotsVisualization.js
@@ -11,19 +11,6 @@ function dotsVisualization()
 	var xAxis;
 	var yAxis;
 	
-	 //Dynamic, random dataset
-	 // TEMPORARY UNTIL DATA AVAILABLE
-	var dataset2 = [];
-	var numDataPoints = 20;
-	var xRange = Math.random() * 1000;
-	var yRange = Math.random() * 1000;
-	for (var i = 0; i < numDataPoints; i++) {
-		var newNumber1 = Math.round(Math.random() * xRange);
-		var newNumber2 = Math.round(Math.random() * yRange);
-		dataset2.push([newNumber1, newNumber2]);
-	}
-	
-
 	this.create = function()
     {
 		svg_selected.selectAll("*").remove();
@@ -31,21 +18,21 @@ function dotsVisualization()
         document.getElementById("sel_viz").style = "display:block;";
         
 		xScale = d3.scale.linear();		
-		xScale.range([circlePadding,w - circlePadding]);
+		
 		
 		yScale = d3.scale.linear();		
-		yScale.range([h - circlePadding,circlePadding]);
+		
 		
 		rScale = d3.scale.linear();
-		rScale.range([2,5]);
+		
 		
 		xAxis = d3.svg.axis();	
 		xAxis.orient("bottom")
-		.ticks(5);
+		.ticks(10);
 		
 		yAxis = d3.svg.axis();
 		yAxis.orient("left")
-		.ticks(5);
+		.ticks(10);
 	
     }
     
@@ -58,89 +45,79 @@ function dotsVisualization()
 		var xName = comboX.getSelectedOption();
 		var yName = comboY.getSelectedOption();
 		var sizeName = comboSize.getSelectedOption();
-		var measureIDx;
-		var measureIDy;
-		var measureIDsize;
 		
-		xScale.domain([0,d3.max(dataset, function(d)
+		xScale.domain(d3.extent(dataset, function(d)
+		{   
+            if(!d.children)
+            {
+                if(d.measures)
+                {
+                    for(var j = 0; j < d.measures.length; j++)
+                    {
+                            if(d.measures[j].name == xName)
+                            {
+                                //console.log( "nro " + parseFloat(d.measures[j].value) + "sem parse" + d.measures[j].value);
+                                return parseFloat(d.measures[j].value);
+                            }
+                    }
+                }
+                
+            }
+	
+		}));
+		xScale.range([circlePadding,w - circlePadding]);
+        
+		yScale.domain(d3.extent(dataset, function(d)
 		{
-			if(d.rank = "7" && d.children)
-			{
-				if(d.children)
+				if(!d.children)
 				{
-					if(d.children[0].measures)
+					if(d.measures)
 					{
-						for(var j = 0; j < d.children[0].measures.length; j++)
+						for(var j = 0; j < d.measures.length; j++)
 						{
-								if(d.children[0].measures[j].name == xName)
+								if(d.measures[j].name == yName)
 								{
-									measureIDx = j;	
-									return parseFloat(d.children[0].measures[j].value);
+									return parseFloat(d.measures[j].value);
+								}
+						}
+					}
+					
+				}
+			
+		}));
+        yScale.range([h - circlePadding,circlePadding]);
+        
+		rScale.domain(d3.extent(dataset, function(d)
+		{
+				if(!d.children)
+				{
+					if(d.measures)
+					{
+						for(var j = 0; j < d.measures.length; j++)
+						{
+								if(d.measures[j].name == sizeName)
+								{
+									return parseFloat(d.measures[j].value);
 								}
 						}
 					}
 					
 				}
 
-			}
-		})]);
-		
-		
-		yScale.domain([0,d3.max(dataset, function(d)
-		{
-			if(d.rank = "7" && d.children)
-			{
-				if(d.children)
-				{
-					if(d.children[0].measures)
-					{
-						for(var j = 0; j < d.children[0].measures.length; j++)
-						{
-								if(d.children[0].measures[j].name == yName)
-								{
-									measureIDy = j;	
-									return parseFloat(d.children[0].measures[j].value);
-								}
-						}
-					}
-					
-				}
-
-			}
-		})]);
-		rScale.domain([0,d3.max(dataset, function(d)
-		{
-			if(d.rank = "7" && d.children)
-			{
-				if(d.children)
-				{
-					if(d.children[0].measures)
-					{
-						for(var j = 0; j < d.children[0].measures.length; j++)
-						{
-								if(d.children[0].measures[j].name == sizeName)
-								{
-									measureIDsize = j;	
-									return parseFloat(d.children[0].measures[j].value);
-								}
-						}
-					}
-					
-				}
-
-			}
-		})]);
-		
+		}));
+		rScale.range([2,10]);
+        
 		xAxis.scale(xScale);
 		yAxis.scale(yScale);
 		
+        /*
 		svg_selected.append("clipPath")
 			.attr("id","chart-area")
 			.append("rect")
 			.attr("x",borderBarPadding)
 			.attr("y",borderBarPadding)
 			.attr("width", w - borderBarPadding*3)
-			.attr("height", h - borderBarPadding*2);
+			.attr("height", h - borderBarPadding*2);*/
 		
 		svg_selected.append("g")
 			.attr("id","circles")
@@ -152,15 +129,47 @@ function dotsVisualization()
 			.filter(function (d){return !d.children})
 			.attr("cx", function(d)
 			{
-				return xScale(parseFloat(d.measures[measureIDx].value));
+                if(d.measures)
+                {
+                    for(var j = 0; j < d.measures.length; j++)
+                    {
+                            if(d.measures[j].name == xName)
+                            {
+                                console.log("X " +xScale(parseFloat(d.measures[j].value)));
+                                return xScale(parseFloat(d.measures[j].value));
+                            }
+                    }
+                }
+				
 			})
 			.attr("cy", function(d)
 			{
-				return yScale(parseFloat(d.measures[measureIDy].value));
+                if(d.measures)
+                {
+                    for(var j = 0; j < d.measures.length; j++)
+                    {
+                            if(d.measures[j].name == yName)
+                            {
+                                //console.log("Y " + yScale(parseFloat(d.measures[j].value))+ " range "+ yScale.range() + " real "+ d.measures[j].value + " dominio " + yScale.domain());
+                                return yScale(parseFloat(d.measures[j].value));
+                            }
+                    }
+                }
+				
 			})
 			.attr("r", function(d)
 			{
-				return rScale(parseFloat(d.measures[measureIDsize].value));
+				if(d.measures)
+                {
+                    for(var j = 0; j < d.measures.length; j++)
+                    {
+                            if(d.measures[j].name == sizeName)
+                            {
+                                return rScale(10);
+                                //return rScale(parseFloat(d.measures[j].value));
+                            }
+                    }
+                }
 			})
 			.attr("fill", function(d){return d.color;});
 			

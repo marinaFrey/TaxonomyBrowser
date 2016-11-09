ccf46daa6093d05aec4eab15cfe0373a830aec14
Marina <mfrey@inf.ufrgs.br>
2016-11-09 00:28:41 -0200

    videos no tutorial

--- js/utilities/tutorial.js
+++ js/utilities/tutorial.js
@@ -12,7 +12,8 @@ function startIntro()
                 intro: "This is the database's hirerarchy representation. <br><br>" +
 						"Here you can left click to zoom in on a partition, right click to select specimens for viewing and hover to manage taxons and add new information! <br><br>"+
 						"In order to zoom out, left click the central circle."+
-						"<p style='text-align:center;'><img src='images/gif_sunburst.gif' width='200' height='200' border='0' title='Specimens represented as little colored circles'></p> <br>",
+						"<video width='300' height='300' autoplay loop><source src='images/sunburst.mp4' type='video/mp4'>Your browser does not support the video tag.</video>",
+						//"<p style='text-align:center;'><img src='images/gif_sunburst.gif' width='300' height='300' border='0' title='Specimens represented as little colored circles'></p> <br>",
 				position: 'right'
               },
 			  {
@@ -27,14 +28,15 @@ function startIntro()
               },
               {
                 element: '#byspecimen',
-                intro: 'This button sets the size of each partition to be proportional to the number of SPECIMENS registered, which gives a good idea of how the database is currently populated.',
+                intro: 'This button sets the size of each partition to be proportional to the number of SPECIMENS registered, which gives a good idea of how the database is currently populated.'+
+						"<video width='300' height='300' autoplay loop><source src='images/troca_particao.mp4' type='video/mp4'>Your browser does not support the video tag.</video> ",
 				position: 'bottom'
               },
 			// filtering  
 			  {
                 element: '#selected_view',
                 intro: "In this section, every specimen selected on the hierarchy will appear as a little colored circle! <br>"+
-						"<p style='text-align:center;'><img src='images/specimen_dots_example.png' width='70' height='70' border='0' title='Specimens represented as little colored circles'></p> <br>"+
+						"<video width='300' height='300' autoplay loop><source src='images/bolinhas.mp4' type='video/mp4'>Your browser does not support the video tag.</video> <br>"+
 						"The color of each circle represents its species color on the hierarchy visualization.",
                 position: 'right'
               },
@@ -51,7 +53,8 @@ function startIntro()
 			  {
                 element: '#filterEditButton',
                 intro: "When clicking this button, a pop-up for managing filters will appear. You can add as many filters as you want! <br><br>"+
-						"After applying them, only the selected specimens that match your filters will be shown below.",
+						"After applying them, only the selected specimens that match your filters will be shown below. "+
+						"<video width='300' height='300' autoplay loop><source src='images/filter.mp4' type='video/mp4'>Your browser does not support the video tag.</video> ",
                 position: 'bottom'
               },
 			  {
@@ -72,17 +75,20 @@ function startIntro()
               },
               {
                 element: '#coord',
-                intro: 'When clicking this button, the Parallel Coordenates visualization will be shown.',
+                intro: 'When clicking this button, the Parallel Coordenates visualization will be shown.'+
+				"<video width='300' height='300' autoplay loop><source src='images/coord.mp4' type='video/mp4'>Your browser does not support the video tag.</video> ",
                 position: 'bottom'
               },
 			  {
                 element: '#dots',
-                intro: 'When clicking this button, the Scatterplot visualization will be shown.',
+                intro: 'When clicking this button, the Scatterplot visualization will be shown.'+
+				"<video width='300' height='300' autoplay loop><source src='images/scatterplot.mp4' type='video/mp4'>Your browser does not support the video tag.</video> ",
                 position: 'bottom'
               },
 			  {
                 element: '#map',
-                intro: 'When clicking this button, a map with the locations of all specimens selected.',
+                intro: 'When clicking this button, a map with the locations of all specimens selected.'+
+				"<video width='300' height='300' autoplay loop><source src='images/mapa.mp4' type='video/mp4'>Your browser does not support the video tag.</video> ",
                 position: 'bottom'
               },
 			// toolbar - por algum motivo nao aparece o escrito direito...
